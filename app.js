c7f100a8dd23d486232370fbc655ffe92a2fc031
nirduk <nirdyk@gmail.com>
2022-06-17 22:10:41 +0300

    add win\loose colors

--- app.js
+++ app.js
@@ -11,103 +11,116 @@ const lizard_div = document.getElementById('lizard')
 const spock_div = document.getElementById('spock')
 
 const getComputerChoice = () => {
-  const choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock'];
+  const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
   const randomNumber = Math.floor(Math.random() * 5);
   return choices[randomNumber];
 }
 
 const getStatus = (userChoice, computerChoice) => {
   switch (userChoice + computerChoice) {
-    case 'RockScissors':
-    case 'RockLizard':
-    case 'ScissorsRock':
-    case 'LizardRock':
+    case 'rockscissors':
+    case 'rocklizard':
+    case 'scissorsrock':
+    case 'lizardrock':
       return 'crushes';
 
-    case 'ScissorsPaper':
-    case 'PaperScissors':
+    case 'scissorspaper':
+    case 'paperscissors':
       return 'cuts';
 
-    case 'ScissorsLizard':
-    case 'LizardScissors':
+    case 'scissorslizard':
+    case 'lizardscissors':
       return 'decapitates';
 
-    case 'SpockScissors':
-    case 'ScissorsSpock':
+    case 'spockscissors':
+    case 'scissorsspock':
       return 'smashes';
 
-    case 'SpockRock':
-    case 'RockSpock':
+    case 'spockrock':
+    case 'rockspock':
       return 'vaporizes';
 
-    case 'LizardSpock':
-    case 'SpockLizard':
+    case 'lizardspock':
+    case 'spocklizard':
       return 'poisons';
 
-    case 'LizardPaper':
-    case 'PaperLizard':
+    case 'lizardpaper':
+    case 'paperlizard':
       return 'eats';
 
-    case 'PaperSpock':
-    case 'SpockPaper':
+    case 'paperspock':
+    case 'spockpaper':
       return 'disproves';
 
-    case 'PaperRock':
-    case 'RockPaper':
+    case 'paperrock':
+    case 'rockpaper':
       return 'covers';
   }
+}
+
+const makeFirstLetterBig = (choice) => {
+  if (choice === 'rock') return 'Rock';
+  if (choice === 'paper') return 'Paper';
+  if (choice === 'scisors') return 'Scissors';
+  if (choice === 'lizard') return 'Lizard';
+  return 'Spock'
 }
 
 const win = (userChoice, computerChoice) => {
   userScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
-  result_p.innerHTML = `${userChoice} ${getStatus(userChoice, computerChoice)} ${computerChoice}. </br>You win!`
+  result_p.innerHTML = `${makeFirstLetterBig(userChoice)} ${getStatus(userChoice, computerChoice)} ${makeFirstLetterBig(computerChoice)}. </br>You win!`;
+  document.querySelector('.score-board').classList.add('green-glow');
 }
 
 const lose = (userChoice, computerChoice) => {
   computerScore++;
   userScore_span.innerHTML = userScore;
   computerScore_span.innerHTML = computerScore;
-  result_p.innerHTML = `${computerChoice} ${getStatus(userChoice, computerChoice)} ${userChoice}. </br>You lose!`
+  result_p.innerHTML = `${makeFirstLetterBig(computerChoice)} ${getStatus(userChoice, computerChoice)} ${makeFirstLetterBig(userChoice)}. </br>You lose!`
+  document.querySelector('.score-board').classList.add('red-glow');
+
 }
 
 const draw = () => {
   result_p.innerHTML = `It's a draw.`
+  document.querySelector('.score-board').classList.add('yellow-glow');
+
 }
 
 const game = (userChoice) => {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
-    case 'RockScissors':
-    case 'RockLizard':
-    case 'ScissorsPaper':
-    case 'ScissorsLizard':
-    case 'SpockScissors':
-    case 'SpockRock':
-    case 'LizardSpock':
-    case 'LizardPaper':
-    case 'PaperSpock':
-    case 'PaperRock':
+    case 'rockscissors':
+    case 'rocklizard':
+    case 'scissorspaper':
+    case 'scissorslizard':
+    case 'spockscissors':
+    case 'spockrock':
+    case 'lizardspock':
+    case 'lizardpaper':
+    case 'paperspock':
+    case 'paperrock':
       win(userChoice, computerChoice);
       break;
-    case 'Rockspock':
-    case 'RockPaper':
-    case 'ScissorsRock':
-    case 'ScissorsSpock':
-    case 'SpockLizard':
-    case 'SpockPaper':
-    case 'LizardScissors':
-    case 'LizardRock':
-    case 'PaperLizard':
-    case 'PaperScissors':
+    case 'rockspock':
+    case 'rockpaper':
+    case 'scissorsrock':
+    case 'scissorsspock':
+    case 'spocklizard':
+    case 'spockpaper':
+    case 'lizardscissors':
+    case 'lizardrock':
+    case 'paperlizard':
+    case 'paperscissors':
       lose(userChoice, computerChoice);
       break;
-    case 'RockRock':
-    case 'ScissorsScissors':
-    case 'Spockspock':
-    case 'LizardLizard':
-    case 'PaperPaper':
+    case 'rockrock':
+    case 'scissorsscissors':
+    case 'spockspock':
+    case 'lizardlizard':
+    case 'paperpaper':
       draw(userChoice, computerChoice);
       break;
   }
@@ -115,23 +128,23 @@ const game = (userChoice) => {
 
 const main = () => {
   rock_div.addEventListener('click', function() {
-    game('Rock')
+    game('rock')
   })
   
   paper_div.addEventListener('click', function() {
-    game('Paper')
+    game('paper')
   })
   
   scissors_div.addEventListener('click', function() {
-    game('Scissors')
+    game('scissors')
   })
   
   lizard_div.addEventListener('click', function() {
-    game('Lizard')
+    game('lizard')
   })
   
   spock_div.addEventListener('click', function() {
-    game('Spock')
+    game('spock')
   })
 }
 
