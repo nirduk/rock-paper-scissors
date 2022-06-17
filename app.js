c4a11822f3b747255917f21b82c38024384077d4
nirduk <nirdyk@gmail.com>
2022-06-17 22:36:35 +0300

    functions -> =>

--- app.js
+++ app.js
@@ -130,25 +130,15 @@ const game = (userChoice) => {
 }
 
 const main = () => {
-  rock_div.addEventListener('click', function() {
-    game('rock')
-  })
-  
-  paper_div.addEventListener('click', function() {
-    game('paper')
-  })
-  
-  scissors_div.addEventListener('click', function() {
-    game('scissors')
-  })
-  
-  lizard_div.addEventListener('click', function() {
-    game('lizard')
-  })
-  
-  spock_div.addEventListener('click', function() {
-    game('spock')
-  })
+  rock_div.addEventListener('click', () => game('rock'));  
+
+  paper_div.addEventListener('click', () => game('paper')); 
+
+  scissors_div.addEventListener('click', () => game('scissor')); 
+
+  lizard_div.addEventListener('click', () => game('lizard')); 
+   
+  spock_div.addEventListener('click', () => game('spock'));
 }
 
 main();
